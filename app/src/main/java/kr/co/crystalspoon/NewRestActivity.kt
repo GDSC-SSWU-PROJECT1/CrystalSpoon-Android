b0a08edb0fc2ea67771041076b51ad5a4ea86d0d
leejw-lu <jiwoojenny@naver.com>
2022-04-06 16:17:02 +0900

    [FEAT] NewRestActivity layout

---
+++ app/src/main/java/kr/co/crystalspoon/NewRestActivity.kt
@@ -0,0 +1,12 @@
+package kr.co.crystalspoon
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class NewRestActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_newrest)
+    }
+}
+
