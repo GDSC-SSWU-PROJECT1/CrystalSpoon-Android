685c384d69c148e91080aaa9a680b04e25e8176c
cloudwoon <cloud.wooo0n@gmail.com>
2022-06-03 18:30:33 +0900

    [feat] review_detail xml 완료

---
+++ app/src/main/java/kr/co/crystalspoon/ReviewDetailActivity.kt
@@ -0,0 +1,11 @@
+package kr.co.crystalspoon
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ReviewDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_review_detail)
+    }
+}
