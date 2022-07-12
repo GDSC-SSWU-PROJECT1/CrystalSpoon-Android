8e9808bfbbf5be550b1ad02abf46454e2156b40f
b1urrrr <b1urrr@naver.com>
2022-07-12 22:30:40 +0900

    [ADD] export 및 바인딩 연결

--- app/src/main/java/kr/co/crystalspoon/WishActivity.kt
+++ app/src/main/java/kr/co/crystalspoon/WishActivity.kt
@@ -2,10 +2,15 @@ package kr.co.crystalspoon
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kr.co.crystalspoon.databinding.ActivityWishBinding
 
 class WishActivity : AppCompatActivity() {
+
+    val binding by lazy { ActivityWishBinding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_wish)
+        setContentView(binding.root)
+
     }
 }
