3d5b7caa6732054af7ae56aeed45b23aa7391534
minyoung kim <55653869+min0k0311@users.noreply.github.com>
2022-07-26 23:06:47 +0900

    [FEAT] add btn back in myInfo, review, setting kt

---
+++ app/src/main/java/kr/co/crystalspoon/WheelActivity.kt
@@ -0,0 +1,20 @@
+package kr.co.crystalspoon
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kr.co.crystalspoon.databinding.ActivityReviewBinding
+import kr.co.crystalspoon.databinding.ActivityWheelBinding
+
+class WheelActivity : AppCompatActivity() {
+
+    val binding by lazy { ActivityWheelBinding.inflate(layoutInflater) }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.ac)
+
+//        binding.wheelBtnBack.setOnClickListener {
+//            finish()
+//        }
+    }
+}
