2f59c946bbc9da69c817144ed35ca654bf6fbd1f
minyoung kim <55653869+min0k0311@users.noreply.github.com>
2022-07-26 22:33:18 +0900

    [FEAT] add btn back in myInfo, review, setting kt

--- app/src/main/java/kr/co/crystalspoon/MyInfoActivity.kt
+++ app/src/main/java/kr/co/crystalspoon/MyInfoActivity.kt
@@ -2,10 +2,19 @@ package kr.co.crystalspoon
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import kr.co.crystalspoon.databinding.ActivityMyInfoBinding
+import kr.co.crystalspoon.databinding.ActivitySettingBinding
 
 class MyInfoActivity: AppCompatActivity() {
+
+    val binding by lazy { ActivityMyInfoBinding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_my_info)
+
+        binding.myInfoBtnBack.setOnClickListener {
+            finish()
+        }
     }
 }
--- app/src/main/java/kr/co/crystalspoon/ReviewActivity.kt
+++ app/src/main/java/kr/co/crystalspoon/ReviewActivity.kt
@@ -2,10 +2,18 @@ package kr.co.crystalspoon
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import kr.co.crystalspoon.databinding.ActivityReviewBinding
 
 class ReviewActivity: AppCompatActivity() {
+
+    val binding by lazy { ActivityReviewBinding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_review)
+
+        binding.reviewBtnBack.setOnClickListener {
+            finish()
+        }
     }
 }
